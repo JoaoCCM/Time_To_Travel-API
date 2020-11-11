c9e4bdef826d76d2d09f43ef18873e7fa0045f63
JoaoCCM <joaocarlosc.m@hotmail.com>
2020-11-11 20:16:12 -0300

    implementing admin middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,24 +1,37 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken')
 
 module.exports = (app) => {
-  const authenticateToken = async (req, res, next) => {
-    try {
-      const access_token = req.headers["authorization"].split(" ")[1];
+    const authenticateToken = async (req, res, next) => {
+        try {
+            const access_token = req.headers['authorization'].split(' ')[1]
 
-      if (access_token === null) return res.status(401).send({ message: "Missing access token" });
+            if (access_token === null)
+                return res.status(401).send({ message: 'Missing access token' })
 
-      jwt.verify(access_token, process.env.JWT_SECRET, (err, user) => {
-        if (err) {
-          return res.status(401).json({ message: "Access Denied" });
+            jwt.verify(access_token, process.env.JWT_SECRET, (err, user) => {
+                if (err) {
+                    return res.status(401).json({ message: 'Access Denied' })
+                }
+
+                req.user = user
+                next()
+            })
+        } catch (error) {
+            throw error
         }
-
-        req.user = user;
-        next();
-      });
-    } catch (error) {
-      throw error;
     }
-  };
 
-  return { authenticateToken };
-};
+    const authenticateAdmin = async (req, res, next) => {
+        try {
+            if (req.user.payload.type !== 'admin') {
+                return res.status(401).json({ message: 'Access Denied' })
+            }
+
+            next()
+        } catch (error) {
+            throw error
+        }
+    }
+
+    return { authenticateToken, authenticateAdmin }
+}
